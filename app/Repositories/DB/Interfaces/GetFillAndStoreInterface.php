83852450113c85c73d778e86c1650110e068eb16
Djalma Manfrin <djalma.manfrin@gmail.com>
2021-03-22 17:04:05 -0300

    configs no bootstrap

--- app/Repositories/DB/Interfaces/GetFillAndStoreInterface.php
+++ app/Repositories/DB/Interfaces/GetFillAndStoreInterface.php
@@ -2,10 +2,10 @@
 
 namespace PayAny\Repositories\DB\Interfaces;
 
-use PayAny\Models\Wallet;
-
-interface GetFillAndStoreInterface
+interface WalletRepositoryInterface
 {
     public function fill(array $values);
+    public function turnValueIntoNegative();
+    public function getFunds(int $walletId): float;
     public function store(): bool;
 }
