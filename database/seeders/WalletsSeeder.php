5c7f33fd2304092161aa14fe39863009f22b5b98
Djalma Manfrin <djalma.manfrin@gmail.com>
2021-03-22 17:15:15 -0300

    WalletsSeeder

--- database/seeders/WalletsSeeder.php
+++ database/seeders/WalletsSeeder.php
@@ -6,13 +6,13 @@ use Illuminate\Database\Seeder;
 use PayAny\Models\User;
 use PayAny\Models\Wallet;
 
-class UsersSeeder extends Seeder
+class WalletsSeeder extends Seeder
 {
     public function run()
     {
-        User::factory()
+        Wallet::factory()
             ->count(10)
-            ->has(Wallet::factory()->count(1))
+            ->has(User::factory()->count(1))
             ->create();
     }
 }
